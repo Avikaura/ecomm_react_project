48366347ee062afac0fd1589a430c22b9e959cd3
Avi Kaura <avikaura20@gmail.com>
2021-07-12 00:00:49 -0400

    Created the filters for the products

---
+++ src/components/Filters.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Filters = () => {
+    return (
+        <section class="filter">
+
+        <legend class="legend-head">colors</legend>
+        <legend class="legend-head">sizes</legend>
+        <legend class="legend-head">ratings</legend>
+        <legend class="legend-head">collections</legend>
+        <legend class="legend-head">materials</legend>
+
+      
+      
+        <legend class="sort">sort</legend>
+      </section>
+    )
+}
+
+export default Filters
