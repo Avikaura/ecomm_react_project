6f23a5790d8d7d2a60963a2d43a4b6d7dd16936b
Avi Kaura <avikaura20@gmail.com>
2021-08-10 22:37:54 -0400

    Images of products with their filtering is in progressive mode

--- src/components/ProductArray.js
+++ src/components/ProductArray.js
@@ -11,23 +11,17 @@ const ProductArray = ({data}) => {
     <article className="product">
     <a href="product.html" target="_blank"><img src={img} alt={title} className="product-img"/></a>  
       <div className="hover-text">quick view</div>
-      <img src={img} alt="fav" className="fav-icon"/>
+      <img src="src/img/svgs/favourite.svg" alt="fav" className="fav-icon"/>
       
       <h4>${bagName}    
           <span><img src="src/img/svgs/star.svg" alt="ratings" className="rating-set"/></span>
           <span><img src="src/img/svgs/star.svg" alt="ratings" className="rating-set"/></span>
           <span><img src="src/img/svgs/star.svg" alt="ratings" className="rating-set"/></span>
           <span><img src="src/img/svgs/star.svg" alt="ratings" className="rating-set"/></span></h4>
-  <data value="479.85" className="price">$${price}</data><br/>
-   <label className="container2">
-      <input type="checkbox"/>
-      <span className="check2"></span>
-    </label>
-    <label className="container3">
-    <input type="checkbox"/>
-    <span className="check3"></span>
-    </label>
+  <data value="479.85" className="price">${price}</data><br/>
+  
      </article>
+     
   )
 } 
 export default ProductArray
