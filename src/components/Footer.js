31c9a0086f30556c8c7f38a5c7cd1b0af273f685
Avi Kaura <avikaura20@gmail.com>
2021-08-10 22:39:13 -0400

    Flitering based on prices is going on

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,10 @@ import what from '../img/svgs/whatsapp.svg'
 const Footer = () => {
     
     return (
+        
         <footer className="footer-part">
+            
+    
         <a href="index.html" className="href-footer"><h1>baggy store</h1></a> 
          <h6>contact</h6>
 
