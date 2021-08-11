8630fb5533418fb11ea7f9c9ecfff8870ec6fde4
Avi Kaura <avikaura20@gmail.com>
2021-08-10 22:38:10 -0400

    Layout App for react is fixed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,18 +2,28 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
+
+
+
 const Layout = ({children}) => {
     return (
         <div className="layout">
-            <Header/>
-            <Menu/>            
-            <Banner />  
-            
-            <main className="products">
-            <section className="result">
+            <Header/>  
+         
+            <main >
             {children}
-            </section>
             </main>
+           
+            <div className="pagination">
+            <a href="#">&laquo;</a>
+            <a class="active" href="#">1</a>
+            <a href="#">2</a>
+            <a href="#">3</a>
+            <a href="#">4</a>
+            <a href="#">5</a>
+            <a href="#">6</a>
+            <a href="#">&raquo;</a>
+        </div>
 
             <Footer />
 
